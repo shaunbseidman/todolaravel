7a08eca7730b327d5ef45991ed0139da5b937a44
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-24 10:52:43 -0600

    created new repo, had issues with mysql v.8 and rolled back to 5.6, moved template.html over

---
+++ laravel/app/Http/Controllers/TodoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(todo $todo)
+    {
+        //
+    }
+}
