22867649b54f699d5069def4cc85ee70976750b8
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-25 16:49:08 -0600

    deleted old employee.blade

--- laravel/resources/views/partials/employees.blade.php
+++ laravel/resources/views/partials/employees.blade.php
@@ -14,18 +14,3 @@
 <label>Assign Task</label>
 </div>
 @endisAdmin
-
-<!-- <div class="input-field col s12">
-  <select name="assignTo">
-    <option value="" disabled selected>Assign to:</option>
-    <option value="{{Auth::user()->id}}">To Myself</option>
-    @foreach($employees as $employee)
-    @if($employee->worker->id == $task->user->id)
-    <option selected value="{{$employee->worker->id}}">{{$employee->worker->name}}</option>
-    @else
-    <option value="{$employee->worker->id}}">{{$employee->worker->name}}</option>
-    @endif
-    @endforeach
-  </select>
-<label>Assign Task</label>
-</div> -->
